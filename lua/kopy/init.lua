5bcbcd856f722798bdcddbcd15f9edc976dae3cf
Martin Kopecký <kopecky@thekrew.app>
2024-04-20 11:38:20 +0200

    More keymaps
    Adding more keymaps and better GUI style

--- lua/kopy/init.lua
+++ lua/kopy/init.lua
@@ -1,9 +1,43 @@
 require("kopy.remap")
 
--- Set line numbers
-vim.cmd('set number')
 -- Set relative line numbers
-vim.cmd('set relativenumber')
+vim.opt.relativenumber = true
+-- Set mouse to work in all modes
+vim.opt.mouse = "a"
 
+-- Set clipboard to system clipboard
+vim.opt.clipboard = "unnamedplus"
 
--- print("hello from kopy")
+-- Set breakindent
+vim.opt.breakindent = true
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Keep sign column open
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Split navigations
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- Scroll buffer
+vim.opt.scrolloff = 10
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight on yank",
+	group = vim.api.nvim_create_augroup("Highlight on yank", {clear = true}),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
--- lua/kopy/remap.lua
+++ lua/kopy/remap.lua
@@ -2,8 +2,23 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 -- Keymaps for external programs
 vim.keymap.set("n", "<F12>", function()
 	vim.cmd("terminal lazygit")
 	vim.cmd("startinsert")
 end, { noremap=true,  silent = true })
+
+
+-- Keymaps for LSP
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Go to previous [D]iagnostic message"})
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Go to next [D]iagnostic message"})
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {desc = "Set [Q]uickfix list"})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {desc = "Open [E]rror diagnostics"})
+
+-- Split navigations
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", {desc = "Move to the left window"})
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", {desc = "Move to the right window"})
+
+
