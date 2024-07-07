e2e38f9d69edda3590bce2c89d47ed8f29720811
Martin Kopecký <kopecky@thekrew.app>
2024-07-07 19:26:19 +0200

    keybind update

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,7 +4,7 @@ local harpoon = require("harpoon")
 harpoon:setup()
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-a>", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
@@ -18,4 +18,4 @@ vim.keymap.set("n", "<F2>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<F3>", function() harpoon:list():next() end)
 
 -- Remove a buffer from the list
-vim.keymap.set("n", "<leader>d", function() harpoon:list():remove() end)
+vim.keymap.set("n", "<C-d>", function() harpoon:list():remove() end)
