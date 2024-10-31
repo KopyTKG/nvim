93ffb9abd54ba6b285c1219eb128321e3cb04757
Martin Kopecký <kopecky@thekrew.app>
2024-10-31 08:32:21 +0100

    updating lsp config

--- init.lua
+++ init.lua
@@ -10,18 +10,49 @@ require("mason").setup({
     }
 })
 
-local lang = {
-}
-
-for _, l in ipairs(lang) do
-	if not require('mason-registry').is_installed(l) then
-    		vim.cmd("MasonInstall " .. l)
-	end
-end
-
 require("mason-lspconfig").setup({
-    -- Enable the LSP server
-    enable = true,
-    -- Enable the LSP server for the following languages
-    languages = lang
+    ensure_installed = {
+        "lua_ls",        -- Lua
+        "rust_analyzer", -- Rust
+        "clangd",        -- C/C++
+        "gopls",         -- Go
+        "pyright",       -- Python
+        "cmake",         -- CMake
+        "ts_ls",      -- TypeScript/JavaScript
+        "html",          -- HTML
+        "eslint",        -- ESLint
+        "tailwindcss",    -- Tailwind CSS
+        "dockerls",      -- Docker
+        "docker_compose_language_service", -- Docker Compose
+        "jsonls",        -- JSON
+        "pylsp",         -- Python LSP (alternative to Pyright)
+        "vimls",         -- Vimscript
+        "ansiblels",     -- Ansible
+        "ast_grep"       -- AST Grep (if supported by lspconfig)
+    },
+    automatic_installation = true,
+})
+
+
+-- Setup lspconfig for each server
+local lspconfig = require("lspconfig")
+require("mason-lspconfig").setup_handlers({
+    function(server_name)  -- Default handler for all servers
+        lspconfig[server_name].setup({
+            capabilities = require('cmp_nvim_lsp').default_capabilities()
+        })
+    end,
+    -- Custom settings for specific servers can be added here
+    ["lua_ls"] = function()
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = {"vim"}
+                    }
+                }
+            },
+            capabilities = require('cmp_nvim_lsp').default_capabilities()
+        })
+    end
 })
